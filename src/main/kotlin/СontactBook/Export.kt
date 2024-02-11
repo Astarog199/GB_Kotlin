99ec564ee263b6ee695d983a44bfb9be93980cdd
pkn <PKN199@yandex.ru>
2024-02-11 22:07:25 +0300

    HomeWork: update

--- src/main/kotlin/СontactBook/Export.kt
+++ src/main/kotlin/СontactBook/Export.kt
@@ -1,11 +1,25 @@
 package main.kotlin.СontactBook
 
+import main.kotlin.UserDB.Write
+import main.kotlin.UserDB.WriteContact
 import java.io.File
 
 
 class Export: Command {
     override fun isValid(): Boolean {
         return true
+    }
+    class Derived (b: Write) : Write by b
+
+    fun safeText(users:  MutableList<Person>) {
+        for (i in users) {
+            val b = WriteContact()
+            Derived(b).writeName(i.getName())
+            Derived(b).writePhone(i.getPhone())
+            Derived(b).writeEmail(i.getMail())
+
+            File("src/main/kotlin/UserDB/", "usersDB.txt").appendText("\n")
+        }
     }
 
     fun run(users:  MutableList<Person>){
